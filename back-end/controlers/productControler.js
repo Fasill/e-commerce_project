fd6bd4f9f5397dbcc4becfa81ace1e0a9a3ec1f8
Fasill <fasilhawultie19@gmail.com>
2023-06-22 16:30:03 +0300

    remove from cart future added

--- back-end/controlers/productControler.js
+++ back-end/controlers/productControler.js
@@ -16,20 +16,6 @@ export const allTraverse = async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 };
-export const cartTraverse = async(req,res)=>{
-  const token = req.body.token;
-  console.log(req.body);
-  const decodedToken = jwt.decode(token);
-  const id = decodedToken.id;
-
-  try{
-    const person = await personSchema.findById(id);
-    const cart = person.cart;
-    const productId = await ProductSchema.findById(cart.productId);
-    res.status(200).json({allcart:productId});
-  }catch(e){console.log(e)};
-
-}
 
 
 export const creatAProduct =async (req,res)=>{
@@ -88,4 +74,93 @@ export const search = async (req,res)=>{
     
     
   }catch(e){console.log(e)}
+}
+
+// cart prosess
+
+export const cartTraverse = async (req, res) => {
+  const token = req.body.token;
+  const decodedToken = jwt.decode(token);
+  const id = decodedToken.id;
+  try {
+    const person = await personSchema.findById(id).populate('cart.productId');
+    const cartProducts = person.cart.map((cartItem) => cartItem.productId);
+
+    res.status(200).json({ cart: cartProducts });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'An error occurred while fetching the cart.' });
+  }
+};
+
+
+export const addToCart = async (req, res) => {
+  const token = req.body.token;
+  console.log("decodding token")
+  const decodedToken = jwt.decode(token);
+  console.log("token decoded")
+  const id = decodedToken.id;
+  console.log(id);
+
+  const { productId, quantity } = req.body;
+
+  try {
+    const person = await personSchema.findById(id);
+    const cartItem = { productId, quantity }; // Create the cart item object
+
+    person.cart.push(cartItem); // Add the cart item to the person's cart array
+    await person.save(); // Save the updated person document
+
+    res.status(200).json({ message: 'Item added to cart successfully.' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: 'An error occurred while adding item to cart.' });
+  }
+};
+
+
+export const removeFromCart = async(req,res)=>{
+  //pass the id in the router
+  const {productId,token} = req.params; 
+
+  console.log(req.params);
+  // const token = req.body.token;
+  console.log("decodding token")
+  const decodedToken = jwt.decode(token);
+  console.log("token decoded")
+  const id = decodedToken.id;
+  console.log(id);
+
+
+  
+    try {
+      // Find the user by their ID
+      const user = await personSchema.findById(id);
+  
+      // Find the index of the cart item with the specified product ID
+      const cartIndex = user.cart.findIndex(item => item.productId.toString() === productId);
+  
+      if (cartIndex === -1) {
+        return res.status(404).json({ message: 'Cart item not found.' });
+      }
+  
+      // Remove the cart item from the user's cart
+      user.cart.splice(cartIndex, 1);
+  
+      // Save the updated user object
+      await user.save();
+  
+      res.status(200).json({ message: 'Cart item deleted successfully.' });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: 'An error occurred while deleting the cart item.' });
+    }
+  };
+  
+
+
+  
+
+export const updateCart = async(req,res)=>{
+
 }
--- back-end/router/productRouter.js
+++ back-end/router/productRouter.js
@@ -1,15 +1,15 @@
 import express from "express";
-import {allTraverse,creatAProduct,search,cartTraverse} from '../controlers/productControler.js'
+import {allTraverse,creatAProduct,search,cartTraverse,addToCart,removeFromCart,updateCart} from '../controlers/productControler.js'
 import {upload} from '../middleware/multer.js'
 
 const productRouter = express.Router();
 
-productRouter.get('/cart',cartTraverse);
-productRouter.get('/',allTraverse);
-productRouter.post('/add',allTraverse);
-productRouter.delete('/remove/:id',allTraverse);
-productRouter.put('/update/;id',allTraverse);
+productRouter.post('/cart',cartTraverse);
+productRouter.post('/cart/add',addToCart);
+productRouter.delete('/cart/remove/:productId/:token',removeFromCart);
+productRouter.put('/cart/update/:productId',updateCart);
 
+productRouter.get('/',allTraverse);
 productRouter.post('/',search);
 productRouter.post('/create',upload.array('images'),creatAProduct);
 
